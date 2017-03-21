556419064a38ff4ccdb66367076b8c640fab34fc
Tyler Hovanec <tbhovanec@gmail.com>
2017-03-21 16:28:05 -0400

    GetGameDetails done

--- src/ServerManager/AvailableGameServerManager.go
+++ src/ServerManager/AvailableGameServerManager.go
@@ -3,6 +3,7 @@ package ServerManager
 import (
 	GameServer "AvailableGamesServer"
 	"golang.org/x/net/context"
+	"log"
 )
 
 // Data type used to implement the AvailableGameServer
@@ -19,15 +20,25 @@ func (s *AvailableGameServer) GetAvailableGamesList(ctx context.Context, in *Gam
 	return &GameServer.GameIdList{ids}, nil
 }
 
-func (s *AvailableGameServer) GetGameDetails(ctx context.Context, in *GameServer.GameId) (*GameServer.GameDetails, error) {
-	var details GameServer.GameDetails
-	details.Console = "nes"
-	details.Id = "0"
-	details.Title = "The Italian Plumber"
-	details.ReleaseDate = "01/12/1990"
-	details.Developer = "Horgan"
-	details.Publisher = "Horgan"
-	details.Genre = "Platformer"
-	details.Players = 2
-	return &details, nil
+func (s *AvailableGameServer) GetGameDetails(ctx context.Context, id *GameServer.GameId) (*GameServer.GameDetails, error) {
+	var details *GameServer.GameDetails
+	
+	err := db.QueryRow("SELECT * FROM GAME WHERE ID = ?", id.GetId()).Scan(details)
+	
+	if err != nil {
+		log.Fatalf("Get game id: %s Error: %s", id.GetId(), err)
+		return details, err
+	}
+	
+	return details, nil
+	
+	//details.Console = "nes"
+	//details.Id = "0"
+	//details.Title = "The Italian Plumber"
+	//details.ReleaseDate = "01/12/1990"
+	//details.Developer = "Horgan"
+	//details.Publisher = "Horgan"
+	//details.Genre = "Platformer"
+	//details.Players = 2
+	//return details, nil
 }
